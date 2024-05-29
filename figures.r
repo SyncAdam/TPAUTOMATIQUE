e54b775529e53f7bfba0a036a606cc2544956380
SyncAdam <adam.sinkovics@gmail.com>
2024-05-29 19:35:21 +0200

    bruhrhruh

--- figures.r
+++ figures.r
@@ -2,10 +2,13 @@ library(ggplot2)
 
 # load data
 data_df <- read.csv("sys1.csv", header = FALSE, skip = 1)
+data_df1 <- read.csv("sys2.csv", header = FALSE, skip = 1)
 
 data_matrix <- as.matrix(data_df)
+data_matrix1 <- as.matrix(data_df1)
 
 data_array <- array(data_matrix, dim = dim(data_matrix))
+data_array1 <- array(data_matrix1, dim = dim(data_matrix1))
 
 a <- 20 * log10(data_array[, 2] / abs(data_array[, 3]))
 points <- cbind(data_array[, 1], a)
@@ -18,32 +21,18 @@ x_breaks <- x_breaks[x_breaks != -60]
 
 x_breaksbode <- c(1, 3, 7.2, 10, 30)
 
-p <- ggplot(as.data.frame(points), aes(x = points[, 1], y = points[, 2])) +
-  geom_point() +
-  geom_line() +
+a <- 20 * log10(data_array1[, 2] / abs(data_array1[, 3]))
+points <- cbind(data_array1[, 1], a)
+points2 <- cbind(data_array1[, 4], a)
+
+p3 <- ggplot(as.data.frame(points), aes(x = points[, 1], y = points[, 2])) +
   labs(x = "fréquence (log)(Hz)", y = "20log(|A|)",
        title = "Diagramme de Bode") +
   scale_y_continuous(breaks = c(seq(-15, 5, by = 5), Threedb)) +
-  scale_x_continuous(trans = "log10", breaks = x_breaksbode) +
+  scale_x_continuous(trans = "log10") +
   geom_hline(yintercept = Threedb,
              linetype = "dashed", color = "red") +
   geom_vline(xintercept = 7.2,
              linetype = "dashed", color = "blue")
 
-p2 <- ggplot(as.data.frame(points2), aes(x = points2[, 1], y = points2[, 2])) +
-  geom_point() +
-  geom_line() +
-  labs(x = "déphasage (degrée)", y = "20log(|A|)",
-       title = "Diagramme de Black") +
-  geom_hline(yintercept = 0,
-             linetype = "dashed", color = "red") +
-  geom_vline(xintercept = -57,
-             linetype = "dashed", color = "blue") +
-  scale_x_continuous(limits = c(-180, 0),
-                     breaks = c(x_breaks, -57))
-
-
-# Print plot
-
-ggsave("bode1.png", plot = p, width = 6, height = 4, units = "in", dpi = 300)
-ggsave("black1.png", plot = p2, width = 6, height = 4, units = "in", dpi = 300)
+ggsave("bode2.png", plot = p3, width = 6, height = 4, units = "in", dpi = 300)
